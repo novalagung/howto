6198adaae72fa57eb1e474a9900e85e13170aff3
novalagung <hello@novalagung.com>
2023-07-21 18:17:36 +0700

    feat: update

--- src/theme/DocItem/Content/index.js
+++ src/theme/DocItem/Content/index.js
@@ -19,7 +19,9 @@ export default function ContentWrapper(props) {
     <>
       <Content {...props} />
       <hr />
-      {window?.location?.pathname === '/' && <SubStack />}
+      <BrowserOnly>
+        {() => window?.location?.pathname === '/' && <SubStack />}
+      </BrowserOnly>
     </>
   );
 }
